7b1f62b0d471119f8edab680c30c890e3e0c22c2
Alex Mejicanos <alexmejicanos@outlook.com>
2017-03-27 13:55:59 -0600

    Paso 2 (Cambiando la URL del UIWebView)

--- Semana4/ViewController.swift
+++ Semana4/ViewController.swift
@@ -11,12 +11,36 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var webView: UIWebView?
+    @IBOutlet var segmentedControl: UISegmentedControl?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let url = URL(string: "http://www.google.com.gt")
         let urlRequest = URLRequest(url: url!)
+        self.webView?.loadRequest(urlRequest)
+    }
+    
+    @IBAction func changeWebPage(sender: UISegmentedControl) {
+        let url: URL
+        switch sender.selectedSegmentIndex {
+        case 0:
+            url = URL(string: "http://www.google.com.gt")!
+            break
+        case 1:
+            url = URL(string: "http://www.apple.com")!
+            break
+        case 2:
+            url = URL(string: "http://www.facebook.com")!
+            break
+        case 3:
+            url = URL(string: "http://www.youtube.com")!
+            break
+        default:
+            url = URL(string: "http://www.google.com.gt")!
+            break
+        }
+        let urlRequest = URLRequest(url: url)
         self.webView?.loadRequest(urlRequest)
     }
 
